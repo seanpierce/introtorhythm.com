ccbd63c5ad64c021779ccc749848beb2f5243be4
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-01 16:40:48 -0700

    CReate Episode model

--- episode/models.py
+++ episode/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Episode(models.Model):
+    id = models.AutoField(primary_key=True)
+    number = models.CharField(max_length=3)
+    title = models.CharField(max_length=250)
+    image = models.CharField(max_length=250)
+    audio = models.CharField(max_length=250)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.number + '- ' + self.title
