bde1efd4120b756f0b21f588b7beaee7fe789926
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-01 16:48:18 -0700

    Register applications

--- introtorhythm/settings.py
+++ introtorhythm/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'episode'
+    'episode',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
