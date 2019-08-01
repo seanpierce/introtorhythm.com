9b8b0b191fbe1d35d041ca9693a3db7a15d5c77d
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-01 16:42:36 -0700

    Render only json for API calls

--- introtorhythm/settings.py
+++ introtorhythm/settings.py
@@ -68,6 +68,12 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
 WSGI_APPLICATION = 'introtorhythm.wsgi.application'
 
 
