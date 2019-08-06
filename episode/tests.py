98827a6979d79b6ee02e7236421f9541510d4074
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-06 13:56:37 -0700

    Add unit tests for repository methods

--- episode/tests.py
+++ episode/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Episode
+from .repository import EpisodeRepository as repo
+
+class EpisodeTestCase(TestCase):
+    def setUp(self):
+        Episode.objects.create(number='001', title='Episode 1')
+        Episode.objects.create(number='002', title='Episode 2')
+        Episode.objects.create(number='003', title='Episode 3')
+
+    def test_retrieve_episode_list(self):
+        """Ensures that episode list is successfully pulled from database"""
+        episode_list = repo.get_episode_list()
+        self.assertEqual(len(episode_list), 3)
+
+    def test_get_current_episode(self):
+        """Ensures that the current episode is successfully pulled from the database"""
+        current_episode = repo.get_current_episode()
+        self.assertEqual('003', current_episode['number'])
+
+    def test_get_current_episode_by_number(self):
+        """Ensures that the current episode is successfully fetched given an episode number"""
+        current_episode = repo.get_current_episode('002')
+        self.assertEqual('002', current_episode['number'])
