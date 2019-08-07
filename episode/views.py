f8d4085c4e16c0f7373fe96293acc9b001f20547
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-06 20:54:00 -0700

    Update index view docstring

--- episode/views.py
+++ episode/views.py
@@ -1,9 +1,22 @@
 import json
 
 from django.shortcuts import render
+
 from .repository import EpisodeRepository as repo
 
-def index(request):
+def index(request, number=None):
+    """Default view for the application.
+    
+    Args:
+        number: An optional argument passed in through the url.
+
+    Returns: 
+        Paginated list of number-title episode data.
+        If a number argument is supplied, and that matches the number of an episode, 
+            a dict containing the matching episide's data will be returned.
+            Else, a 404 page will be rendered.
+    """
+
     data = {
         'episodes': repo.get_episode_list(),
         'current_episode': repo.get_current_episode()
