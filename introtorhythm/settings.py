9760ed83f90db6b591c268ec4bf62b883d7e20a6
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-06 22:16:19 -0700

    Add config parser

--- introtorhythm/settings.py
+++ introtorhythm/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import os.path
+import configparser
+
+CONFIG = configparser.RawConfigParser()
+CONFIG.read('../settings.ini')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
