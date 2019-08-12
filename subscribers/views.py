a78f59d28f84067eadcb498e50beb82ce028f2fa
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-12 09:20:53 -0700

    Add documentation for the confirm API

--- subscribers/views.py
+++ subscribers/views.py
@@ -6,6 +6,26 @@ from django.shortcuts import redirect
 from .repository import SubscriberRepository as repo
 
 def confirm(request, token):
+    """The confirm method is a view that operates as a simple GET api method.
+    
+    Process:
+        * find a subscription request which matches the token provided
+        * create a subscriber based on the data collected in the the
+            subscription request
+        * delete the original subscription request
+        * redirect the user to the main page, with a query string indicating
+            success or failure
+
+    Path: 
+        /api/requests/confirm/[token]
+
+    Args:
+        token: a string value representing a token found in a subscription request record.
+
+    Returns:
+        Returns a redirect response which includes a url parameter indicating 
+        success or failure of the process.
+    """
     subscription_request = repo.get_request_by_token(token)
 
     if subscription_request is None:
