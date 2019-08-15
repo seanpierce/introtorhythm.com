d121bc90fa2ea4056f7b7958a83c244600dc7e0c
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-14 22:41:30 -0700

    Add navigation header

--- assets/js/components/Navigation.vue
+++ assets/js/components/Navigation.vue
@@ -2,10 +2,22 @@
     <div>
         <div id="nav" v-if="loaded">
             <ul>
+                <li><a href="/">Intro To Rhythm</a><li>
+                <li>------</li>
+                <li>Now Playing: Ep {{ currentEpisode.number }}</li>
+                <li id="tracktime">00:00:00 / 00:00:00</li>
+                <li>
+                    <span class="see-info pointer">
+                        See Ep {{ currentEpisode.number }} Info &lt;
+                    </span>
+                </li>
+                <li>------</li>
                 <li 
                     v-for="episode in episodes" 
                     v-bind:key="episode.number">
-                    <a :href="'/' + episode.number">{{ episode.number }}- {{ episode.title }}</a>
+                    <a :href="'/' + episode.number" v-bind:class="{ active: episode.number === currentEpisode.number }">
+                        {{ episode.number }}- {{ episode.title }}
+                    </a>
                 </li>
             </ul>
         </div>
