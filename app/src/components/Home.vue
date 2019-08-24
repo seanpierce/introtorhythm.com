d3866ce8a1ba9e9f1b66d511cd923e25dc3ac3b5
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-08-24 14:01:45 -0700

    Pass data into vue app

---
+++ app/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <h1>Intro To Rhythm</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+
+  })
+}; 
+</script>
+
+<style>
+* {
+  color: red;
+}
+</style>
--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,20 @@
+/*eslint no-console: ["error", { allow: ["log"] }] */
+
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  data: () => ({
+    data: null
+  }),
+  methods: {
+  },
+  mounted() {
+    var elem = document.getElementById('data');
+    var data = elem.attributes.data.value;
+    this.data = JSON.parse(data);
+  }
 }).$mount('#app')
