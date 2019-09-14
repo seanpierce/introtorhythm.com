f6d483d7ac15f10b26f53d01cb03783c9f4feae5
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-09-13 22:29:17 -0700

    Update about modal verbiage

--- assets/app/components/partials/modals/About.js
+++ assets/app/components/partials/modals/About.js
@@ -10,15 +10,21 @@ export var About = {
     template: `
         <div id="about">
             <div class="content">
-                Intro To Rhythm is a freeform podcasting series and live-streaming station that began in 2017. All episodes and content are owned by their creators and made exclusively for Intro To Rhythm.<br>
+                Intro To Rhythm is a freeform mix series and live-streaming audio station that began in 2017. All episodes and content are owned by their creators and made exclusively for Intro To Rhythm.
                 <br>
-                Additionally, ITR is dedicated to open-source projects, and the collective idea sharing that fuels creative expression. If you're interested in developing your own live-streaming station or podcasting platform, you're invited to refer to ITR's source code (Licence MIT).<br>
+                <br>
+                Additionally, ITR is dedicated to open-source projects, and the collective idea sharing that fuels creative expression. If you're interested in developing your own live-streaming station or podcasting platform, you're invited to refer to ITR's source code (Licence MIT).
+                <br>
                 <br>
                 Send questions and comments to introtorhythm@gmail.com<br>
                 Follow ITR on: <a href="https://www.facebook.com/introtorhythm/">Facebook</a> / <a href="https://www.instagram.com/introtorhythm/">Instagram</a> / <a href="https://twitter.com/introtorhythm">Twitter</a>
                 <br>
                 <br>
-                Maintaining the application and server environments can be costly and time consuming. If you appreciate ITR and you have the means, please consider making a donation.<br>
+                ------------
+                <br>
+                <br>
+                Maintaining this application and it's server environments can be costly and time consuming. If you appreciate ITR and you have the means, please consider making a donation.
+                <br>
                 <br>
                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                     <input type="hidden" name="cmd" value="_s-xclick" />
