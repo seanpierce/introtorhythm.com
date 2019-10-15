41f1288a9572408c06cf72744d8c05ca5c19ee04
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-10-14 21:10:58 -0700

    Add new modal types to modal component

--- app/components/partials/modals/Modal.js
+++ app/components/partials/modals/Modal.js
@@ -1,10 +1,14 @@
 import { EpisodeInfo } from './EpisodeInfo.js';
 import { About } from './About.js';
+import { Episodes } from './Episodes.js';
+import { Subscribe } from './Subscribe.js';
 
 export var Modal = {
     components: {
         'EpisodeInfo': EpisodeInfo,
-        'About': About
+        'About': About,
+        'Episodes': Episodes,
+        'Subscribe': Subscribe,
     },
     props : {
         type: String
@@ -23,6 +27,12 @@ export var Modal = {
 
             if (this.type === "about")
                 this.$parent.showAboutModal = false;
+
+            if (this.type === "episodes")
+                this.$parent.showEpisodesModal = false;
+
+            if (this.type === "subscribe")
+                this.$parent.showSubscribeModal = false;
         }
     },
     computed: {
@@ -30,13 +40,15 @@ export var Modal = {
     template: `
         <div id="modal" @click="closeModal($event)">
             <div class="modal-content">
-                <img id="close" 
-                    class="pointer" 
+                <img id="close"
+                    class="pointer"
                     @click="closeModal($event)"
                     :src="$root.staticUrl + '/images/close.svg'">
                 <!-- content -->
                 <EpisodeInfo v-if="type==='episodeInfo'" />
                 <About v-if="type==='about'" />
+                <Episodes v-if="type==='episodes'" />
+                <Subscribe v-if="type==='subscribe'" />
             </div>
         </div>
     `
