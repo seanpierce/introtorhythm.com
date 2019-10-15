a5b2bae580b90dacc949bbd9f9c35d4cb4b08c35
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-10-14 21:11:54 -0700

    Pudate player to have internal play/pause button

--- app/components/partials/Player.js
+++ app/components/partials/Player.js
@@ -5,41 +5,41 @@ export var Player = {
     },
     methods: {
         getPlayerWidth() {
-            var player = document.getElementById('player');
-            var playhead = document.getElementById('playhead');
-            var playerWidth = player.offsetWidth - playhead.offsetWidth;
+            var player = document.getElementById('timeline');
+            var playHead = document.getElementById('playHead');
+            var playerWidth = player.offsetWidth - playHead.offsetWidth;
             this.$parent.playerWidth = playerWidth;
         },
         clickEvent(event) {
-            var player = document.getElementById('player');
+            var player = document.getElementById('timeline');
             var percent = (event.pageX - player.offsetLeft) / this.$parent.playerWidth;
             this.$parent.audio.currentTime = this.$parent.audio.duration * percent;
         },
         mouseDownEvent() {
-            this.$parent.onplayhead = true;
-            window.addEventListener('mousemove', this.movePlayhead, true);
+            this.$parent.onplayHead = true;
+            window.addEventListener('mousemove', this.movePlayHead, true);
             window.addEventListener('mouseup', this.mouseUpEvent, false);
             this.$parent.audio.removeEventListener('timeupdate', this.$parent.timeUpdate, false);
         },
         mouseUpEvent() {
-            this.$parent.onplayhead = false;
+            this.$parent.onplayHead = false;
             this.$parent.audio.addEventListener('timeupdate', this.$parent.timeUpdate, true);
-            window.removeEventListener('mousemove', this.movePlayhead, true);
+            window.removeEventListener('mousemove', this.movePlayHead, true);
             window.removeEventListener('mouseup', this.mouseUpEvent, true);
         },
-        movePlayhead(event) {
-            var player = document.getElementById('player');
-            var playhead = document.getElementById('playhead');
+        movePlayHead(event) {
+            var player = document.getElementById('timeline');
+            var playHead = document.getElementById('playHead');
             var margin = event.pageX - player.offsetLeft;
-      
+
             if (margin >= 0 && margin <= this.$parent.playerWidth)
-                playhead.style.marginLeft = margin + "px";
-      
+                playHead.style.marginLeft = margin + "px";
+
             if (margin < 0)
-                  playhead.style.marginLeft = "0px";
-      
+                  playHead.style.marginLeft = "0px";
+
             if (margin > this.$parent.playerWidth)
-                playhead.style.marginLeft = this.$parent.playerWidth + "px"
+                playHead.style.marginLeft = this.$parent.playerWidth + "px"
         },
     },
     computed: {
@@ -48,24 +48,27 @@ export var Player = {
         }
     },
     mounted() {
-        var player = document.getElementById('player');
-        var playhead = document.getElementById('playhead');
-    
+        var player = document.getElementById('timeline');
+        var playHead = document.getElementById('playHead');
+
         this.getPlayerWidth();
-    
+
         player.addEventListener('click', (event) => {
             this.clickEvent(event);
         });
-    
-        playhead.addEventListener('mousedown', this.mouseDownEvent, false);
+
+        playHead.addEventListener('mousedown', this.mouseDownEvent, false);
         window.addEventListener('resize', this.getPlayerWidth);
     },
     template: `
         <div id="player" v-if="loaded">
-            <div id="audio-player">
-            <div id="timeline">
-                <div id="playhead" :style="{ marginLeft: $parent.playPercent + 'px' }"></div>
-            </div>
+            <div>
+                <div id="controls" @click="$parent.togglePlay()">
+                    <img :src="$parent.playing ? '/assets/images/pause-icon.png' : '/assets/images/play-icon.png'">
+                </div>
+                <div id="timeline">
+                    <div id="playHead" :style="{ marginLeft: $parent.playPercent + 'px' }"></div>
+                </div>
             </div>
         </div>
     `
