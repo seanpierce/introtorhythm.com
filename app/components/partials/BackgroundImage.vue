9a1e3d17fc592501cc09a1ed2368861ee24b0600
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-10-16 13:40:05 -0700

    Update ticker and resolve missing background image

--- app/components/partials/BackgroundImage.vue
+++ app/components/partials/BackgroundImage.vue
@@ -1,29 +1,16 @@
 <template>
-
+    <div>
+        <div id="brighten"></div>
+        <div id="background-image" :style="{ backgroundImage: 'url(' + imageUrl + ')' }"></div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    computed: {
+        imageUrl() {
+            return this.$root.mediaUrl + this.$parent.currentEpisode.image;
+        }
+    },
 }
 </script>
-
-// export var BackgroundImage = {
-//     data() {
-//         return {
-//         }
-//     },
-//     methods: {
-//     },
-//     computed: {
-//         imageUrl() {
-//             return this.$root.mediaUrl + this.$parent.currentEpisode.image;
-//         }
-//     },
-//     template: `
-//         <div>
-//             <div id="brighten"></div>
-//             <div id="background-image" :style="{ backgroundImage: 'url(' + imageUrl + ')' }"></div>
-//         </div>
-//     `
-// }
--- app/components/partials/Ticker.vue
+++ app/components/partials/Ticker.vue
@@ -1,17 +1,22 @@
 <template>
-    <div class="ticker-wrap">
-        <div class="ticker">
-            <div class="ticker__item">&#9873;</div>
-            <div class="ticker__item">Intro To Rhythm</div>
-            <div class="ticker__item">&#9873;</div>
-            <div class="ticker__item">Episode: {{ currentEpisode.number }} - {{ currentEpisode.title }}</div>
-            <div class="ticker__item">&#9873;</div>
-            <div class="ticker__item">{{ $parent.currentTime }} / {{ $parent.totalTime || '00:00:00' }}</div>
-            <div class="ticker__item">&#9873;</div>
-            <div class="ticker__item">introtorhythm@gmail.com</div>
-            <div class="ticker__item">&#9873;</div>
-            <div class="ticker__item">{{ localTime }}</div>
-            <div class="ticker__item">&#9873;</div>
+    <div>
+        <div id="logo-spinner">
+            <img :src="$root.staticUrl + '/images/itr-logo.png'">
+        </div>
+        <div class="ticker-wrap">
+            <div class="ticker">
+                <div class="ticker__item">&#9873;</div>
+                <div class="ticker__item">Intro To Rhythm</div>
+                <div class="ticker__item">&#9873;</div>
+                <div class="ticker__item">Episode: {{ currentEpisode.number }} - {{ currentEpisode.title }}</div>
+                <div class="ticker__item">&#9873;</div>
+                <div class="ticker__item">{{ $parent.currentTime }} / {{ $parent.totalTime || '00:00:00' }}</div>
+                <div class="ticker__item">&#9873;</div>
+                <div class="ticker__item">introtorhythm@gmail.com</div>
+                <div class="ticker__item">&#9873;</div>
+                <div class="ticker__item">{{ localTime }}</div>
+                <div class="ticker__item">&#9873;</div>
+            </div>
         </div>
     </div>
 </template>
