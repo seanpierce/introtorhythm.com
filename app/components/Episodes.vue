559355d4043bdc81e9340867713106244133eb00
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-11-29 12:48:12 -0800

    FE updates

--- app/components/Episodes.vue
+++ app/components/Episodes.vue
@@ -1,15 +1,27 @@
 <template>
-    <div id="episodes" class="content">
-        <div class="cover-filter"></div>
-        <div v-if="latestEpisode"
-            id="latest-episode"
-            :style="{ backgroundImage: 'url(' + $root.mediaUrl + latestEpisode.image + ')' }">
-            <div class="show-info">
-                <div class="title">{{ latestEpisode.number }}- {{ latestEpisode.title }}</div>
-                <div class="show-time">{{ $root.localTime }}</div>
+    <div id="episodes">
+        <div class="content">
+            <div class="cover-filter"></div>
+            <div v-if="latestEpisode"
+                id="latest-episode"
+                :style="{ backgroundImage: 'url(' + $root.mediaUrl + latestEpisode.image + ')' }">
+                <div class="show-info">
+                    <div class="title">{{ latestEpisode.number }}- {{ latestEpisode.title }}</div>
+                    <div class="show-time">{{ $root.localTime }}</div>
+                </div>
+                <div class="play-button" @click="play()">
+                    <img :src="playing ? '/assets/images/pause.png' : '/assets/images/play.png'" alt="">
+                </div>
             </div>
-            <div class="play-button" @click="play()">
-                <img :src="playing ? '/assets/images/pause.png' : '/assets/images/play.png'" alt="">
+        </div>
+        <div class="sub-content">
+            <div v-if="episodes" class="section">
+                <h1>Episodes</h1>
+                <EpisodesGrid :episodes="episodes"
+                    :paginate="12" />
+            </div>
+            <div>
+                <img src="/assets/images/itr-text.png" class="itr-text">
             </div>
         </div>
     </div>
@@ -17,8 +29,12 @@
 
 <script>
 import axios from 'axios';
+import EpisodesGrid from './partials/EpisodeGrid.vue';
 
 export default {
+    components: {
+        EpisodesGrid
+    },
     data() {
         return {
             episodes: null,
@@ -29,8 +45,8 @@ export default {
         getEpisodes() {
             axios.get('/api/episodes')
                 .then(response => {
+                    this.latestEpisode = response.data.shift();
                     this.episodes = response.data;
-                    this.latestEpisode = this.episodes[0];
                     this.setAudioSrouce();
                 })
         },
--- app/components/partials/EpisodeGrid.vue
+++ app/components/partials/EpisodeGrid.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>
-        <div v-for="episode in episodes"
-            :key="episode.id"
-            class="episode-container">
-            <div class="episode-wrapper">
-                <div class="episode"
-                    :style="{ backgroundImage: 'url(' + $root.mediaUrl + episode.image + ')' }">
-                    <div class="episode-content">
-                        <div>{{ episode.number }}<br>{{ episode.title }}</div>
+    <div class="episodes-grid">
+        <div v-if="!loaded" style="text-align:center">
+            <em>LOADING</em>
+        </div>
+        <div v-if="loaded">
+            <div v-for="episode in showEpisodes"
+                :key="episode.id"
+                class="episode-container"
+                @click="goToEpisode(episode.number)">
+                <div class="episode-wrapper">
+                    <div class="episode"
+                        :style="{ backgroundImage: 'url(' + $root.mediaUrl + episode.image + ')' }">
+                        <div class="episode-content">
+                            <div>{{ episode.number }}<br>{{ episode.title }}</div>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div class="show-more-button" v-if="showShowMore" @click="showMore()">
+                Show more
             </div>
         </div>
     </div>
@@ -18,7 +27,58 @@
 <script>
 export default {
     props: [
-        'episodes'
-    ]
+        'episodes',
+        'paginate'
+    ],
+    data() {
+        return {
+            imageCount: 0,
+            loadedImages: 0,
+            showEpisodes: []
+        }
+    },
+    methods: {
+        preloadImages() {
+            var images = this.episodes.map(x => x.image);
+            this.imageCount = images.length;
+            images.forEach(img => {
+                var image = new Image();
+                image.onload = () => {
+                    this.loadedImages ++;
+                }
+                image.src = this.$root.mediaUrl + img;
+            });
+        },
+        initialShowEpisodes() {
+            if (!this.paginate) this.showEpisodes = this.episodes;
+            else {
+                this.showEpisodes = this.episodes.slice(0, this.paginate);
+            }
+        },
+        showMore() {
+            var startAt = this.showEpisodes.length;
+            var take = this.paginate + startAt;
+            var add = this.episodes.slice(startAt, take);
+            this.showEpisodes = this.showEpisodes.concat(add);
+        },
+        goToEpisode(number) {
+            window.location.href = '/episodes/' + number;
+        }
+    },
+    computed: {
+        loaded() {
+            if (this.imageCount === 0) return false;
+            if (this.imageCount === this.loadedImages)
+                return true;
+            else return false;
+        },
+        showShowMore() {
+            return this.episodes.length !== this.showEpisodes.length;
+        }
+    },
+    mounted() {
+        this.initialShowEpisodes();
+        this.preloadImages();
+    }
 }
 </script>
