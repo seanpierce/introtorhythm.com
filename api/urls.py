9160046a1483f2312410ec74b8230ce4daa0bec6
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-12-07 15:28:39 -0800

    Add routing for new APIs

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
 from .episodes_api import Recent, All, Search, Episode
+from .content_api import Info
+from .subscribers_api import ConfirmSubscription, RequestSubscription, Unsubscribe
 
 urlpatterns = [
     path('episodes/recent', Recent.as_view()),
     path('episodes/search', Search.as_view()),
     path('episodes/<number>', Episode.as_view()),
     path('episodes', All.as_view()),
+    path('content/<name>', Info.as_view()),
+    path('subscribers/request', RequestSubscription.as_view()),
+    path('subscribers/confirm/<token>', ConfirmSubscription.as_view()),
+    path('subscribers/unsubscribe', Unsubscribe.as_view()),
 ]
