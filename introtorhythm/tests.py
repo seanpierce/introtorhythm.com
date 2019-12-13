d5a9b760401f470f5c742717e01d64520dfa2d31
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-12-13 13:53:04 -0800

    Add generic tests to main app

---
+++ introtorhythm/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+class IntroToRhythmTestCase(TestCase):
+
+    def test_index_route(self):
+        response = self.client.get('/', follow=True)
+        self.assertTemplateUsed(response, 'app.html')
+
+    def test_episodes_route(self):
+        response = self.client.get('/episodes', follow=True)
+        self.assertTemplateUsed(response, 'app.html')
+
+    def test_episode_route(self):
+        response = self.client.get('/episodes/002', follow=True)
+        self.assertTemplateUsed(response, 'app.html')
