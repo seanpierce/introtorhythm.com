1a9bb2a61f630c117c3a3f02c2d18d0c4afe5e11
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-12-19 12:17:11 -0800

    Add AWS media url property

--- introtorhythm/settings.py
+++ introtorhythm/settings.py
@@ -171,8 +171,9 @@ STATICFILES_DIRS = [os.path.join('assets'),]
 AWS_ACCESS_KEY_ID = CONFIG.get('AWS Secret Keys', 'AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = CONFIG.get('AWS Secret Keys', 'AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'podcasts.introtorhythm.com'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
+AWS_MEDIA_URL = 'https://s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
