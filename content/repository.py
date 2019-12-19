817779c77d899de24ff7339a8233c03c1aaf4f9c
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-12-19 12:16:31 -0800

    Update repo doc strings

--- content/repository.py
+++ content/repository.py
@@ -1,10 +1,11 @@
 from .models import Content
 
 class ContentRepository(object):
-    """Access layer for content data."""
+    """Access layer for reading and writing Content data."""
 
     @staticmethod
     def get_content_by_name(name):
+        """Returns content data when provided a name alias."""
         content = Content.objects.filter(active=True, name__iexact=name).first()
         return {
             'id': content.id,
