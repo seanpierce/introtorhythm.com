65a74bb9347247dae582aa5937d0c2785125c004
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-12-19 15:48:38 -0800

    Sort episodes in django admin

--- episodes/admin.py
+++ episodes/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Episode
 
-# class EpisodeAdmin(admin.ModelAdmin)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    search_fields = ('number','title','content')
+    list_filter = ('active','created_at')
 
-admin.site.register(Episode)
+admin.site.register(Episode, EpisodeAdmin)
