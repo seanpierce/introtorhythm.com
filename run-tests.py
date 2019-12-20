9a9e91b833a3c7473353861c488d86d0fbcf1ff2
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-12-19 17:34:30 -0800

    Add coverage and tracvis ci scripts

---
+++ run-tests.py
@@ -0,0 +1,8 @@
+import os
+
+# run tests with coverage
+os.system('coverage run manage.py test')
+
+# generate coverage badge
+os.system('rm coverage.svg || true')
+os.system('coverage-badge -o coverage.svg')
