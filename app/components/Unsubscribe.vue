d90e4be45cd8b48dd3955725c04a69065a96a0a2
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-12-20 11:53:50 -0800

    Add unsubscribe component

--- app/components/Unsubscribe.vue
+++ app/components/Unsubscribe.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>Unsubscribe</div>
+    <div id="unsubscribe">
+        <h1>Unsubscribe</h1>
+        <div id="form">
+            <div>Enter your email below to remove yourself from our mailing list.</div>
+            <input type="email" placeholder="your@email.com" />
+            <button>Submit</button>
+        </div>
+        <div><a href="/">Return to ITR</a></div>
+    </div>
 </template>
 
 <script>
