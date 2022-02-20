4b9ed6ec12efc85ba3ac0f1dfbd2db046094829b
Sean Pierce <sumler.sean@gmail.com>
2022-02-20 13:34:36 -0800

    Add method to check header secrets
    Remove csrf exempt decorator from class

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,13 @@
 import json
+from django.conf import settings
 from django.http import HttpResponse
 from django.views.generic import View
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 
-@method_decorator(csrf_exempt, name='dispatch')
+
 class APIView(View):
+    """
+    Base class for custom API view.
+    """
     
     def Response(self, data, status=200):
         try:
@@ -22,3 +24,28 @@ class APIView(View):
             payload[param] = body[param]
 
         return payload
+
+    
+    def secret_is_valid(self, key, request):
+        """
+        Looks for a header secret matching the name of a supplied key.
+        If found, validates that the value of the header matches what is in the 
+        application's configuration.
+        """
+
+        if key is None:
+            return False
+
+        # example SCHEDULE_AUTH_SECRET
+        configured_key = '%s_SECRET' %(key.replace('-', '_'))
+        configured_secret = getattr(settings, configured_key, None)
+        if configured_secret is None:
+            return False
+
+        try:
+            # example X-SCHEDULE-AUTH-SECRET
+            secret = request.headers['X-%s-SECRET' %(key)]
+            valid = secret == configured_secret
+            return valid
+        except Exception as ex:
+            return False
