163d4c7a2736734c380e336bd2b4280047f9203a
Sean Pierce <sumler.sean@gmail.com>
2022-02-20 09:34:57 -0800

    Add actions to scheduled shows admin area

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-
 from .models import Show
 
 class ShowAdmin(admin.ModelAdmin):
     search_fields = ['title', 'info']
     list_filter = ['active']
     list_display = ['title', 'date', 'start_time']
-    list_filter = ['active']
+    actions = ['mark_shows_active', 'mark_shows_inactive']
+
+    def mark_shows_active(self, request, queryset):
+        queryset.update(active=True)
+
+    def mark_shows_inactive(self, request, queryset):
+        queryset.update(active=False)
 
 admin.site.register(Show, ShowAdmin)
