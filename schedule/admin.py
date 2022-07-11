a5abbdbdd12d8cd087d4599cd189b16209ffc136
Sean Pierce <sumler.sean@gmail.com>
2022-07-11 13:10:48 -0700

    Create custom filter for showing upcoming and past shows

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,9 +1,68 @@
+import datetime
 from django.contrib import admin
+from django.utils.encoding import force_text
 from .models import Show
 
-class ShowAdmin(admin.ModelAdmin):
+class UpcomingFilter(admin.SimpleListFilter):
+    title = 'Upcoming'
+    parameter_name = 'upcoming'
+    default_value= 'Upcoming'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('Upcoming', 'Upcoming'),
+            ('Past', 'Past'),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+
+        # Compare the requested value (either 'Upcoming' or 'Past')
+        # to decide how to filter the queryset.
+
+        if self.value() == None:
+            self.used_parameters[self.parameter_name] = 'Upcoming'
+
+        if self.value() == 'Upcoming':
+            return queryset.filter(
+                date__gte = datetime.date.today()
+            )
+        if self.value() == 'Past':
+            return queryset.filter(
+                date__lte = datetime.date.today()
+            )
+
+    def choices(self, changelist):
+        """
+        Override to remiove the 'All' option for this filter.
+        """
+
+        yield {
+            'selected': self.value() is None,
+            'query_string': changelist.get_query_string({}, [self.parameter_name]),
+        }
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == force_text(lookup),
+                'query_string': changelist.get_query_string({self.parameter_name: lookup}, []),
+                'display': title,
+            }
+
+
+class UpcomingShowAdmin(admin.ModelAdmin):
     search_fields = ['title', 'info']
-    list_filter = ['active']
+    list_filter = [UpcomingFilter, 'active']
     list_display = ['title', 'date', 'start_time']
     actions = ['mark_shows_active', 'mark_shows_inactive']
 
@@ -13,4 +72,4 @@ class ShowAdmin(admin.ModelAdmin):
     def mark_shows_inactive(self, request, queryset):
         queryset.update(active=False)
 
-admin.site.register(Show, ShowAdmin)
+admin.site.register(Show, UpcomingShowAdmin)
