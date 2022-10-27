59ed6918238b64acecf688a34b22fbde2b3d53f0
Sean Pierce <sumler.sean@gmail.com>
2022-10-27 09:46:02 -0700

    Show images in schedule page

--- app/src/components/ScheduleCard.vue
+++ app/src/components/ScheduleCard.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="schedule-card">
-        {{ date }} at {{ startTime }} - {{ show.title }}
+        {{ show.title }}<br>
+        <span class="show-date">{{ date }} at {{ startTime }}</span>
+
+        <div v-if="image">
+            <img class="show-image" :src="image" :alt="show.title">
+        </div>
     </div>
 </template>
 
 <script>
 import moment from 'moment'
+const mediaUrl = process.env.VUE_APP_MEDIA_URL
 
 export default {
     props: {
@@ -21,6 +27,13 @@ export default {
 
         startTime() {
             return moment(this.show.start_time, 'h').format('hh:mm a')
+        },
+
+        image() {
+            if (!this.show.show_image)
+                return null
+            
+            return `${mediaUrl}${this.show.show_image}`
         }
     }
 }
