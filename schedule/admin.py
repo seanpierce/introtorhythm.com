477ebba2871cb9a1e378ef8ca440acde73370352
Sean Pierce <sumler.sean@gmail.com>
2022-11-17 11:59:38 -0800

    Add end time and image preview to list view for shows

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,6 +1,8 @@
 import datetime
+from .helpers import TIMES
 from django.contrib import admin
 from django.utils.encoding import force_text
+from django.utils.html import format_html
 from .models import Show
 
 class UpcomingFilter(admin.SimpleListFilter):
@@ -63,14 +65,41 @@ class UpcomingFilter(admin.SimpleListFilter):
 class UpcomingShowAdmin(admin.ModelAdmin):
     search_fields = ['title', 'info']
     list_filter = [UpcomingFilter, 'active']
-    list_display = ['title', 'date', 'start_time']
+    list_display = ['title', 'date', 'start_time', 'get_formatted_end_time', 'image_tag']
     actions = ['mark_shows_active', 'mark_shows_inactive']
-    exclude = ['start_date_time', 'end_date_time']
+    exclude = ['start_date_time', 'end_date_time', 'image_tag']
 
     def mark_shows_active(self, request, queryset):
         queryset.update(active=True)
 
     def mark_shows_inactive(self, request, queryset):
         queryset.update(active=False)
+
+    def get_formatted_end_time(self, obj):
+        """
+        Returns the hour of the end_date_time field.
+        Value is formatted using the TIMES helper.
+        ex: if the hour is 18, field would show 6 pm.
+        """
+        return TIMES[obj.end_date_time.hour][1]
+
+    get_formatted_end_time.short_description = 'End Time'
+
+    def image_tag(self, obj):
+        img_html = """
+            <img src="{}" 
+                onclick="window.open('{}', '_blank')"
+                style="cursor:pointer;max-height:150px;max-width:150px;" />
+        """
+        try:
+            if obj is not None and obj.show_image:
+                return format_html(img_html.format(obj.show_image.url, obj.show_image.url))
+            else:
+                return 'Not available'
+        except:
+            return 'Not available'
+
+    image_tag.short_description = 'Image'
+
 
 admin.site.register(Show, UpcomingShowAdmin)
