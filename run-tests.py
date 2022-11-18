439ea61c88e6ad7df1f57336f7f81aa36cf5fa4c
Sean Pierce <sumler.sean@gmail.com>
2022-11-18 12:22:49 -0800

    Remove old cd/cd files

--- run-tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-
-# run tests with coverage
-os.system('coverage run manage.py test')
-
-# generate coverage badge
-os.system('rm coverage.svg || true')
-os.system('coverage-badge -o coverage.svg')
