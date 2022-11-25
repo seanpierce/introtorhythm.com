a61d492df0671e3133e29b619df904caae6043ca
Sean Pierce <sumler.sean@gmail.com>
2022-11-25 15:22:57 -0800

    Pull file into endpoint and parse name to find show

--- api/live_recordings_api.py
+++ api/live_recordings_api.py
@@ -1,7 +1,9 @@
 import json
 from django.http import HttpResponse
 from django.views.generic import View
+from .api_helper import ApiHelper
 from .decorators import header_auth
+from repositories.schedule import ScheduleRepository
 
 
 class ProcessLiveRecording(View):
@@ -10,9 +12,15 @@ class ProcessLiveRecording(View):
     shceudled show in the database. If found, the process
     reconfigures ezstream to run the scheduled show's file.
     """
-    
+
     @header_auth('PROCESS_LIVE_RECORDING_AUTH', 'X-PROCESS-LIVE-RECORDING-AUTH-SECRET')
     def post(self, request):
 
-        payload = json.loads(request.body)
-        return HttpResponse(json.dumps(payload), content_type='application/json')
+        file = request.FILES['recording']
+        # get date and hour of show
+        date_string = ApiHelper.get_date_string_from_filename(file.name)
+        hour = ApiHelper.get_hour_from_filename(file.name)
+        # reference shows table to get content
+        show = ScheduleRepository.get_show_by_date_and_hour(date_string, hour)
+        # save live recording to db
+        return HttpResponse(json.dumps(show), content_type='application/json')
