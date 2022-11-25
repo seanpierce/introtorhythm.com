7026a0fad61649c89e1998c3080f06d0d6147028
Sean Pierce <sumler.sean@gmail.com>
2022-11-25 15:22:09 -0800

    Add helper to parse filename into date and hour

--- api/api_helper.py
+++ api/api_helper.py
@@ -15,3 +15,35 @@ class ApiHelper:
         config_secret = config.get('Secrets', secret)
         header_secret = request.request.headers[header]
         return config_secret == header_secret
+
+
+    @staticmethod
+    def get_date_string_from_filename(filename:str) -> str:
+        """
+        ex: ITR_rec_20221123-090448.mp3
+        """
+
+        output = filename.replace('ITR_rec_', '')
+        output = output.split('-')[0]
+        output = f"{output[:4]}-{output[4:6]}-{output[6:]}"
+        return output
+
+
+    @staticmethod
+    def get_hour_from_filename(filename:str) -> int:
+        """
+        ex: ITR_rec_20221123-090448.mp3
+        """
+
+        output = filename.replace('ITR_rec_', '')
+        output = output.split('-')
+
+        hour = int(output[1][:2])
+        minutes = int(output[1][2:4])
+
+        # set threshold for rounding up if
+        # the stream started slightly before the hour
+        if minutes > 50:
+            hour = hour + 1
+
+        return hour
