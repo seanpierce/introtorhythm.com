0dd938f6939ace35b3244cef3ddf354c87d8422b
ssumler <ssumler@i-ology.com>
2023-01-20 11:58:25 -0800

    Add expiration date to episode admin list

--- episodes/admin.py
+++ episodes/admin.py
@@ -5,7 +5,7 @@ class EpisodeAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ['number','title','content']
     list_filter = ['active','featured','created_at']
-    list_display = ['active', 'number', 'title']
+    list_display = ['active', 'number', 'title', 'expiration_date']
     list_display_links = ['title']
     actions = ['mark_episodes_active', 'mark_episodes_inactive']
     exclude = ['expiration_date']
