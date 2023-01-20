9a0b1c95941c8d44af2d745b3306a23708e96c02
ssumler <ssumler@i-ology.com>
2023-01-20 11:52:11 -0800

    Update episode model to create expiration conditionally
    Also adds pre and post save events

--- episodes/models/episode.py
+++ episodes/models/episode.py
@@ -5,7 +5,9 @@ import os
 from ckeditor.fields import RichTextField
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
 from PIL import Image
+
 
 class Episode(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,7 +20,7 @@ class Episode(models.Model):
     audio = models.FileField(upload_to = 'episodes/audio/', max_length = 500, default = None)
     active = models.BooleanField(default=True)
     featured = models.BooleanField(default=False)
-    expiration_date = models.DateTimeField(blank=True, null=True, default=datetime.datetime.now() + datetime.timedelta(days=30))
+    expiration_date = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         ordering = ['-number']
@@ -44,5 +46,93 @@ class Episode(models.Model):
         image.save(buffer, "JPEG")
         buffer.seek(0)
         bucket.put_object(Key=filename, Body=buffer, ACL='public-read')
+
+
+@receiver(models.signals.pre_save, sender=Episode)
+def pre_save(sender, instance, **kwargs):
+    """
+    Method that is executed before the instance is saved to the database.
+    """
+    set_expiration(instance)
+    auto_delete_image_on_change(instance)
+    auto_delete_audio_on_change(instance)
+
+
+@receiver(models.signals.post_delete, sender=Episode)
+def post_delete(sender, instance, using, **kwargs):
+    """
+    Method that is executed after the instance is deleted from the database.
+    """
+    delete_image_when_instance_is_deleted(instance)
+    delete_audio_when_instance_is_deleted(instance)
+
+
+def set_expiration(instance):
+    """
+    Updates the episode's expiration date based on its active and featured properties.
+    """    
+    is_featured = instance.featured
+    is_active = instance.active
+
+    if is_active and not is_featured:
+        instance.expiration_date = datetime.datetime.now() + datetime.timedelta(days = 60)
+    else:
+        instance.expiration_date = None
+
+
+def auto_delete_image_on_change(instance):
+    """
+    Deletes old image from filesystem (AWS S3)
+    when corresponding `Image` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return None
+
+    try:
+        old_file = Episode.objects.get(pk=instance.pk).image
+    except:
+        return None
+
+    new_file = instance.image
+
+    if not old_file == new_file:
+        old_file.delete(save=False)
+
+
+def auto_delete_audio_on_change(instance):
+    """
+    Deletes old audio from filesystem (AWS S3)
+    when corresponding `Image` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return None
+
+    try:
+        old_file = Episode.objects.get(pk=instance.pk).audio
+    except:
+        return None
+
+    new_file = instance.audio
+
+    if not old_file == new_file:
+        old_file.delete(save=False)
+
+
+def delete_image_when_instance_is_deleted(instance):
+    """
+    Deletes image from filesystem (AWS S3)
+    when corresponding `Image` record is deleted.
+    """
+    instance.image.delete(save=False)
+
+
+def delete_audio_when_instance_is_deleted(instance):
+    """
+    Deletes audio from filesystem (AWS S3)
+    when corresponding `Image` record is deleted.
+    """
+    instance.audio.delete(save=False)
 
         
