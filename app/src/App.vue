ac545a269df779cad7b2d93cda10ee78a762d199
seanpierce <sumler.sean@gmail.com>
2024-02-07 14:23:55 -0800

    Remove unused files

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,32 +3,13 @@
     <div id="container">
       <router-view />
     </div>
-    <Player />
   </div>
 </template>
 
 <script>
-import Player from '@/components/Player'
 
 export default {
-
-  components: {
-    Player
-  },
-
-  data() {
-    return {
-      modal: null
-    }
-  },
-
   computed: {
-
-    showLogo() {
-      const options = ['Episode', 'Live']
-      return options.includes(this.$route.name)
-    },
-
     showChat() {
       return this.$route.name === 'Chat'
     }
@@ -36,10 +17,6 @@ export default {
 
   created() {
     this.$store.dispatch('initialize')
-    setInterval(() => {
-      // poll for new background images
-      this.$store.dispatch('pollRefreshContent')
-    }, 120000) // two minutes
   }
 }
 </script>
