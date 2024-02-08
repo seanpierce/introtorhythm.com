0b125c3d43ffc41c26b57ec052080cd85e668984
seanpierce <sumler.sean@gmail.com>
2024-02-08 14:45:07 -0800

    Update chat styles

--- app/src/components/Chat/Chat.vue
+++ app/src/components/Chat/Chat.vue
@@ -1,49 +1,52 @@
 <template>
 <div id="chat">
-    <div 
-        class="new-user"
-        v-if="!chat.username">
-
-        <div class="new-user-form">
-            Please enter a username
-            <input 
-                placeholder="Chester"
-                type="text" 
-                autofocus
-                minlength="4"
-                maxlength="20"
-                @keyup.enter="setUser()"
-                v-model="newUser">
-            <button @click="setUser()">Join</button>
-
-            <div 
-                class="helper" 
-                v-bind:class="{ warning: newUserError }">
-                Usernames may only contain letters, numbers, underscores, or hyphens
+    <div id="chat-wrapper">
+        <div 
+            class="new-user"
+            v-if="!chat.username">
+    
+            <div class="new-user-form">
+                Please enter a username
+                <input 
+                    placeholder="Chester"
+                    type="text" 
+                    autofocus
+                    minlength="4"
+                    maxlength="20"
+                    @keyup.enter="setUser()"
+                    v-model="newUser">
+                <button @click="setUser()">Join</button>
+    
+                <div 
+                    class="helper" 
+                    v-bind:class="{ warning: newUserError }">
+                    Usernames may only contain letters, numbers, underscores, or hyphens
+                </div>
             </div>
         </div>
-    </div>
-
-    <div v-else>
-        <div id="messages">
-            <Message 
-                v-for="(message, index) in chat.messages" :key="index"
-                :message="message"
-                :myMessage="message.username === chat.username" />
+    
+        <div v-else>
+            <div id="messages-header">
+                <span>Username: <strong>{{ chat.username }}</strong></span> | <strong><span class="clickable" @click="logout()">Logout</span></strong>
+            </div>
+            <div id="messages">
+                <Message 
+                    v-for="(message, index) in chat.messages" :key="index"
+                    :message="message"
+                    :myMessage="message.username === chat.username" />
+            </div>
+    
+            <div id="message-input">
+                <input 
+                    type="text" 
+                    maxlength="140"
+                    @keyup.enter="submit()"
+                    v-model="message"
+                    placeholder="Say something">
+    
+                <button class="submit" @click="submit()">Submit</button>
+            </div>
         </div>
-
-        <div id="message-input">
-            <input 
-                type="text" 
-                maxlength="140"
-                @keyup.enter="submit()"
-                v-model="message"
-                placeholder="Say something">
-
-            <button class="submit" @click="submit()">Submit</button>
-        </div>
-
-        <div id="logout" @click="logout()">Logout</div>
     </div>
 </div>
 </template>
