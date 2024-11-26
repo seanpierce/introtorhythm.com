6fb1479448f587e8e3bd0ce400c312d908d0e360
seanpierce <sumler.sean@gmail.com>
2024-11-26 09:45:49 -0800

    Update form to post in new tab

--- app/src/components/Info.vue
+++ app/src/components/Info.vue
@@ -9,7 +9,7 @@
                 &#8681;
             </div>
             <div>
-                <form action="https://www.paypal.com/donate" method="post" target="_top">
+                <form action="https://www.paypal.com/donate" method="post" target="_blank">
                     <input type="hidden" name="hosted_button_id" value="WUK6JYHURH6KJ" />
                     <input id="donate" type="submit" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" value="DONATE TO I2R">
                     <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
